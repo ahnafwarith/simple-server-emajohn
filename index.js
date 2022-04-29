97cb7ce671d0fc032905a15aea777b18909e18e9
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-30 04:48:13 +0600

    created a simple server

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config()
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b2hlo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('emaJohn').collection('product')
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Coding with node')
+})
+
+app.listen(port, () => {
+    console.log('listening quietly to port', port)
+})
